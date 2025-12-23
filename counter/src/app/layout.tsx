5ffa3955e24563259c4002124d29a354285d787a
RadhikaSuryawanshi <radhikasuryawanshi279@gmail.com>
2025-12-23 13:20:22 +0530

    installed react-tostify

--- counter/src/app/layout.tsx
+++ counter/src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
-//import 'react-toastify'
 import { ToastContainer } from "react-toastify";
 
 const geistSans = Geist({
